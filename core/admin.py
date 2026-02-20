95c2798dc84efc42a34ad37bebb53c4f4f603971
LeonardoMarquis <marquisleonardo12@gmail.com>
2026-02-19 21:56:06 -0300

    relacionamenteos entre models

--- core/admin.py
+++ core/admin.py
@@ -5,3 +5,20 @@ from .models import Produto
 @admin.register(Produto)
 class ProdutoAdmin(admin.ModelAdmin):
     list_display = ('nome', 'preco', 'estoque', 'slug', 'criado', 'modificado', 'ativo')
+
+
+# 
+from .models import Montadora, Carro, Chassi
+
+@admin.register(Montadora)
+class MontadoraAdmin(admin.ModelAdmin):
+    list_display = ('nome',)
+
+@admin.register(Chassi)
+class ChassiAdmin(admin.ModelAdmin):
+    list_display = ('numero',)
+    # para opython 111 é int, 111, é tupla, (111) é int, (111,) é tupla
+
+@admin.register(Carro)
+class CarroAdmin(admin.ModelAdmin):
+    list_display = ('modelo', 'preco', 'chassi')
--- core/models.py
+++ core/models.py
@@ -29,3 +29,48 @@ def produto_pre_save(signal, instance, sender, **kwargs):
 # quando o produto for salvo, ele vai gerar o slug
 # ex o Maria Mole vai virar maria-mole
 signals.pre_save.connect(produto_pre_save, sender=Produto)
+
+
+
+# ===============================
+
+class Chassi (models.Model):
+    numero = models.CharField('Numero do Chassi', max_length=16, help_text='Máximo 16 caracteres')
+
+    class Meta:
+        verbose_name = 'Chassi'
+        verbose_name_plural = 'Chassis'
+    
+    def __str__(self):
+        return self.numero
+    
+class Montadora(models.Model):
+    nome = models.CharField('Nome da Montadora', max_length=100)
+
+    class Meta:
+        verbose_name = 'Montadora'
+        verbose_name_plural = 'Montadoras'
+    
+    def __str__(self):
+        return self.nome
+
+class Carro (models.Model):
+
+    # models.OneToOneField
+    # um para um
+
+    # models.ForeignKey
+    # um para muitos, tipo uma montadora para muitos carros
+
+
+    chassi = models.OneToOneField(Chassi, on_delete=models.CASCADE)     # esse one to one é para dizer que o relacionamento de carro, vai ter 1 chassi e cada chassi so vai ter 1 carro
+    montadora = models.ForeignKey(Montadora, on_delete=models.CASCADE)
+    modelo = models.CharField('Modelo', max_length=100)
+    preco = models.DecimalField('Preço', max_digits=8, decimal_places=2)
+
+    class Meta:
+        verbose_name = 'Carro'
+        verbose_name_plural = 'Carros'
+
+        def __str__(self):
+            return f"{self.montadora}-{self.modelo}"
